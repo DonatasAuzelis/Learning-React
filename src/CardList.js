ca636ba72f7f1f815a7ad5ee1f5d7f7ca9cc6fb3
Donatas Auzelis <donatas963@gmail.com>
2022-01-20 10:03:31 +0100

    Added cardlist component to loop and list cards

---
+++ src/CardList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+
+    return (
+
+        <div>
+        {
+            robots.map((robot, id) => {
+                return (
+                    <Card
+                    key={id}
+                    id={robots[id].id}
+                    name={robots[id].name}
+                    email={robots[id].email}
+                    />
+                )
+            } )
+        }
+        </div>
+        
+    )
+
+}
+
+export default CardList;
