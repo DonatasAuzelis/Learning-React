a7b35a1d7062e4f1141d080851e63fe8d718832d
Donatas Auzelis <donatas963@gmail.com>
2022-01-20 09:47:36 +0100

    modified card component and added data for robots

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({name, email, id}) => {
+
     return(
-        <div className='bg-light-green dib br3 pa3 ma2 grow'>
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <h1>Robo Friend</h1>
-            <img alt='robot' src='https://robohash.org/1?200x200' />
+            <img alt='robot' src={`https://robohash.org/${id}?200x200`} />
             <div>
-                <h1>Firstname Lastname</h1>
-                <p>email@email.com</p>
+                <h1>{name}</h1>
+                <p>{email}</p>
             </div>
         </div>
     )
