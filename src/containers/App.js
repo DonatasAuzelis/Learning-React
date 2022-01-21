659d6297198fe9ca1934420139f677e712f3ba14
Donatas Auzelis <donatas963@gmail.com>
2022-01-21 15:53:22 +0100

    organized files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
-import CardList from "./CardList"
-import Searchbar from "./Searchbar"
-import Scroll from './Scroll'
+import CardList from "../components/CardList"
+import Searchbar from "../components/Searchbar"
+import Scroll from '../components/Scroll'
 import './App.css'
 
 class App extends Component{
